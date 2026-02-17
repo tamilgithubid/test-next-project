3bccac32a3e750dc9496873a8c109e32a84c0d46
tamilgithubid <tamilarasansundarraj@gmail.com>
2026-02-17 12:42:47 +0530

    feat: add UserCard component and update Home page to display user information

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,9 @@
-import Image from "next/image";
+import UserCard from "./UserCard";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <div className="flex flex-col items-center gap-4">
-        <h1 className="text-4xl font-bold text-gray-800 dark:text-gray-200">
-          Welcome to Next.js 14!
-        </h1>
-        <p className="text-lg text-gray-600 dark:text-gray-400">
-          This is your new Next.js app. Start building something amazing!
-        </p>
-       <p className="text-sm text-gray-500 dark:text-gray-500">
-        HII
-        </p>
-   <div className="mt-6">
-    Hello World
-  </div>
-      </div>
+      <UserCard />
     </div>
   );
 }
